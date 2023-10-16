ab4df740d42ca168452ac8ac7ac192419f0baa7e
Miles <98649838+Blvcked@users.noreply.github.com>
2023-10-16 21:53:34 +0200

    testing

---
+++ cherrypush.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Ensure commit hash argument is passed
+if [ -z "$1" ]; then
+    echo "Error: Commit hash missing."
+    exit 1
+fi
+
+# Fetch changes from upstream
+git fetch upstream
+
+# Create and checkout new branch based on upstream/main
+git checkout -b "cherry-$1" upstream/main
+
+# Cherry-pick the commit
+git cherry-pick $1
+
+# Push to upstream main
+git push upstream main
+
+# Delete the temporary branch
+git branch -D "cherry-$1"
