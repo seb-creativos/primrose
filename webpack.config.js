8cce141863aa508bbb3322fc158c67510598eb87
Miles <98649838+Blvcked@users.noreply.github.com>
2023-10-19 22:16:52 +0200

    Vite to webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const WebpackBar = require("webpackbar");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: "./src/js/app.js",
+	output: {
+		filename: "app.min.js",
+	},
+
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "node_modules"),
+		},
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
+			},
+		],
+	},
+
+	plugins: [
+		new WebpackBar(),
+		new MiniCssExtractPlugin({
+			filename: "app.min.css",
+		}),
+	],
+
+	performance: {
+		maxAssetSize: 512000,
+		maxEntrypointSize: 1280000,
+	},
+};
