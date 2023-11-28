10960d5aacc32f09ddfdc755a84158665f2bb177
Rodrigo Galiana <rodrigo@sebcreativos.es>
2023-11-28 13:38:48 +0100

    area gallery (wip)

--- templates/area-content.php
+++ templates/area-content.php
@@ -1,12 +1,12 @@
 <?php
-    $images = get_post_meta( get_the_ID(), 'area-images', true );
-    $landing = get_post_meta( get_the_ID(), 'area-landing', true );
+$landing = get_post_meta( get_the_ID(), 'area-landing', true );
 ?>
 <section class="area mb-100">
     <div class="container-wide py-50">
         <div class="row gx-md-50">
             <?php get_template_part( 'templates/area-sidebar' ) ?>
             <div class="area__content col-md">
+                <?php get_template_part( 'templates/area-gallery' ) ?>
                 <h1 class="text-primary my-30"><?php the_title() ?></h1>
                 <?php the_content() ?>
                 <a href="<?= $landing ?>" class="btn btn-link has-icon mt-50">
